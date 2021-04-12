4b5c79ea5ec6352a617e662dab802ff833565cd4
Turchenko Anton <turchenkoav@gmail.com>
2021-04-12 18:35:46 +0300

    task#34 Creade responsive mobile navbar with js Implemented opening a mobile menu using js. Implemented a fix for the position when opening the mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,18 @@
-function myFunction() {
-    const x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
+function openCloseNavMenu() {
+    console.log("yay");
+    const x = document.getElementById("nav-menu");
+    if (x.className === "menu") {
         x.className += " responsive";
+        window.onscroll = function () {
+            window.scrollTo(0, 0);
+        };
     } else {
-        x.className = "topnav";
+        x.className = "menu";
+        allowScroll()
     }
+}
+
+function allowScroll() {
+    window.onscroll = function () {
+    };
 }
