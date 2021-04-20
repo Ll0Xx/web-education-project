0071c0500ba6cdc3331e5f24dd19b947090692c4
Turchenko Anton <turchenkoav@gmail.com>
2021-04-20 09:11:54 +0300

    Implemented images carousel

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,38 @@ function allowScroll() {
     window.onscroll = function () {
     };
 }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+
+    const slides = document.getElementsByClassName("worker-container");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+
+    // const dots = document.getElementsByClassName("dot");
+    // for (i = 0; i < dots.length; i++) {
+    //     dots[i].className = dots[i].className.replace(" active", "");
+    // }
+    // dots[slideIndex-1].className += " active";
+}
