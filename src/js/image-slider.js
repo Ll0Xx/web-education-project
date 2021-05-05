72237d6e380a78d446cd135c436e76bf9144013a
Turchenko Anton <turchenkoav96@gmail.com>
2021-05-05 18:00:20 +0300

    Working on slider dots

--- src/js/image-slider.js
+++ src/js/image-slider.js
@@ -1,22 +1,30 @@
 // https://codepen.io/killate/pen/XZNgNX
 //
-(function ($) {
+(function (options) {
 
-    $.makeSlider = function ($carouselElement) {
+    $.makeSlider = function ($carouselElement, options) {
+
+        var settings = $.extend({
+            // These are the defaults.
+            color: "#556b2f",
+            backgroundColor: "white"
+        }, options );
 
         let className = $($carouselElement).attr("class");
 
+        $(window).resize(function () {
+            console.log("yay resized")
+        });
+
         const goToSlide = function (slide) {
-            const $container = $(".workers-container");
             const $slides = $(".worker-container");
 
             if (slide === 1) {
                 const $lastElement = $slides.last();
 
                 $slides.animate({
-                    left: $lastElement.width() + parseInt( $lastElement.css("marginLeft") ) * 2
+                    left: $lastElement.width() + parseInt($lastElement.css("marginLeft")) * 2
                 }, 300, function () {
-                    console.log("yay done left");
                     $slides.css('left', '0');
                     $lastElement.css('opacity', '1');
                     $lastElement.parent().prepend($lastElement);
@@ -25,9 +33,8 @@
                 const $firstElement = $slides.first();
 
                 $slides.animate({
-                    left: -($firstElement.width() + parseInt( $firstElement.css("marginLeft") ) * 2)
+                    left: -($firstElement.width() + parseInt($firstElement.css("marginLeft")) * 2)
                 }, 300, function () {
-                    console.log("yay done right");
                     $slides.css('left', '0');
                     $firstElement.css('opacity', '1');
                     $firstElement.parent().append($firstElement);
@@ -35,9 +42,23 @@
             }
         };
 
-        let makeDots = function () {
+        let makeControlElements = function () {
             $($carouselElement).append('<div class="' + className + '-prev prev"><a></a></div><div class="' + className + '-next next"><a></a></div>');
         };
+
+        let makeDots = function () {
+            var numberOfSlides = 4
+
+            $($carouselElement).append('<div class="' + className + '-dots dots">');
+
+            for (let i = 0; numberOfSlides > i; i++) {
+                if (i === 0) {
+                    $("." + className + "-dots").append("<div class='dot active slide-" + (i + 1) + "'></div>");
+                } else {
+                    $("." + className + "-dots").append("<div class='dot slide-" + (i + 1) + "'></div>");
+                }
+            }
+        }
 
         const next = function () {
             goToSlide(1);
@@ -57,15 +78,20 @@
             });
         };
 
-        makeDots();
+        makeControlElements();
+        if (settings.showDots === true) {
+            makeDots()
+        }
         watchers();
     }
 
     $.fn.makeSlider = function () {
         return this.each(function () {
-            (new $.makeSlider(this));
+            (new $.makeSlider(this, options));
         });
     };
 })(jQuery);
 
-$(".workers-container").makeSlider();
+$(".workers-container").makeSlider({
+    showDots: true
+});
